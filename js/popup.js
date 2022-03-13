f7064e588d44e4ea2be052074abad92fda71d7fc
Amro Shohoud <shohouda@gmail.com>
2022-03-13 13:36:17 -0500

    add privacy policy

---
+++ js/popup.js
@@ -0,0 +1,12 @@
+(function($) {
+    "use strict"; // Start of use strict
+    $(".trigger_popup_fricc").click(function(){
+       $('.hover_bkgr_fricc').show();
+    });
+    $('.hover_bkgr_fricc').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+    $('.popupCloseButton').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+})(jQuery); // End of use strict
